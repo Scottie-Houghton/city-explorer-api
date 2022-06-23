4ec8cfc598641e5ae16322f7b8696f84f17e0d2c
Scottie <scott.c.houghton@gmail.com>
2022-06-22 23:46:22 -0700

    Installed Axios. Added the Weatherbit API key to my .env. Attempted to grab data using the Weatherbit API and rendering it to my site by adding to my GET.

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 'user strict';
 
 // console.log('my first server');
-
-const express = require('express');
 require('dotenv').config();
-let data = require('./data/weather.json');
-
+const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+
+// let data = require('./data/weather.json');
 
 
 const app = express();
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3002;
 
 
 app.get('/', (request, response) => {
-  response.send('hello from our server!');
+  response.status(200).send('hello from our server!');
 });
 
 // app.get('/hello', (request, response) => {
@@ -26,10 +26,21 @@ app.get('/', (request, response) => {
 //   response.send(`Hello, ${name}!`);
 // });
 
-app.get('/weather', (request, response) => {
+// app.get('/daily', async (request, response) => {
+//   let searchQueryFromFrontEnd = request.query.searchQuery;
+//   let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${searchQueryFromFrontEnd}&key=${process.env.WEATHERBIT_API_KEY}`;
+//   let results = await axios.get(url);
+//   console.log(results.data);
+//   response.sendStatus('hi');
+// });
+
+app.get('/weather', async (request, response) => {
   try {
     let queryCity = request.query.searchQuery;
-    let cityData = data.find(object => object.city_name.toLowerCase() === queryCity.toLowerCase());
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${queryCity}&key=${process.env.WEATHERBIT_API_KEY}`;
+    let results = await axios.get(url);
+
+    let cityData = results.find(object => object.city_name.toLowerCase() === queryCity.toLowerCase());
     let dailyForecast = cityData.data.map(day => new Forecast(day));
     response.status(200).send(dailyForecast);
   } catch (error) {
@@ -38,7 +49,7 @@ app.get('/weather', (request, response) => {
 });
 
 app.get('*', (request, response) => {
-  response.send('The thing you are looking for doesn\'t esist');
+  response.status(404).send('The thing you are looking for doesn\'t esist');
 });
 
 
